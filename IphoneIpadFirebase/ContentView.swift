c01e387b7611289a1f1475d9d308c86aa1bdeae7
Josias Pina <pina.jisrael@gmail.com>
2024-02-04 13:43:43 -0800

    Initial Commit

---
+++ IphoneIpadFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IphoneIpadFirebase
+//
+//  Created by Josias Piña on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IphoneIpadFirebase/IphoneIpadFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneIpadFirebaseApp.swift
+//  IphoneIpadFirebase
+//
+//  Created by Josias Piña on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct IphoneIpadFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
